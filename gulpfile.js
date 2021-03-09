21356f89efcbf9a6b3c5ace221bfb27ecbd6775d
Juan Manuel Boudry Leon <buleboudry117@gmail.com>
2021-03-09 16:58:21 +0100

    compilando SASS con gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 //aplicamos destructuring basicamente extraer la función de series de gulp
-const { series, parallel } = require('gulp'); //trae las dependencias de node_modules, require sirve para importar
+// const { series, parallel } = require('gulp'); //trae las dependencias de node_modules, require sirve para importar
 
 // function hola( done ) {
 //     console.log('Hola mundo en gulp');
@@ -15,19 +15,48 @@ const { series, parallel } = require('gulp'); //trae las dependencias de node_mo
 // exports.hola1 = hola;
 
 
-function css (done) {
-    console.log('compilando SASS');
+// function css (done) {
+//     console.log('compilando SASS');
 
-    done();
+//     done();
+// }
+
+// function javascript( done ) {
+//     console.log('compilando javascript');
+
+//     done();
+// }
+
+// exports.compilarSASS = css;
+// exports.javascript = javascript;
+// exports.tareas = series(css,javascript); //si en vez de llamarlo tareas lo llamas default, al poner gulp te lo ejecuta
+// exports.default = parallel(css,javascript); //con parallel lo ejecuta todo en paralelo
+
+const { series,src,dest } = require('gulp'); //la función src lo que hace es buscar los arhcivos de SASS
+                                            //la funcion destnos va a tomar la ruta en la que se va a guardar la versión compilada de esta hoja de estilos o de cualquier otra tarea ( imagenes, scripts)
+const sass = require('gulp-dart-sass'); //como este solo tiene una función va sin llaves
+
+//función que compila SASS
+
+function css(  ) {
+  return src('src/scss/app.scss')
+    .pipe( sass({
+        outputStyle: 'expanded' //para que salga menos junto todo
+        //también le puedes poner 'compressed' y lo deja todo muy reducido lo que lo hace más ligero
+    })) //con esto lees el archivo .scss y lo pasas a SASS pero necesitas un sitio en el que guardarlo
+    .pipe( dest('./build/css')) //marcamos donde queremos guardarlo
 }
 
-function javascript( done ) {
-    console.log('compilando javascript');
 
-    done();
+function minificarcss() { //así podrías tener una función que te minificara el código de forma automática
+    return src('src/scss/app.scss')
+    .pipe( sass({
+        outputStyle: 'compressed' 
+        
+    })) 
+    .pipe( dest('./build/css')) 
+
 }
 
-exports.compilarSASS = css;
-exports.javascript = javascript;
-exports.tareas = series(css,javascript); //si en vez de llamarlo tareas lo llamas default, al poner gulp te lo ejecuta
-exports.default = parallel(css,javascript); //con parallel lo ejecuta todo en paralelo
+exports.css = css;
+exports.minificarcss = minificarcss;
